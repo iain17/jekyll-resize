7ff168b52f2b53cdaa5d6e2f87e37bf515cb20f1
Calum Iain Munro <iain_munro@apple.com>
2024-12-09 12:44:32 +0100

    Add webp support

--- lib/jekyll-resize.rb
+++ lib/jekyll-resize.rb
@@ -6,43 +6,36 @@ module Jekyll
     CACHE_DIR = "cache/resize/"
     HASH_LENGTH = 32
 
-    # Generate output image filename.
     def _dest_filename(src_path, options, dest_dir)
       hash = Digest::SHA256.file(src_path)
       short_hash = hash.hexdigest()[0, HASH_LENGTH]
       options_slug = options.gsub(/[^\da-z]+/i, "")
-      ext = File.extname(src_path)
-
-      "#{short_hash}_#{options_slug}#{ext}"
+      "#{short_hash}_#{options_slug}.webp"
     end
 
-    # Build the path strings.
     def _paths(repo_base, img_path, options)
       src_path = File.join(repo_base, img_path)
       raise "Image at #{src_path} is not readable" unless File.readable?(src_path)
 
       dest_dir = File.join(repo_base, CACHE_DIR)
-
       dest_filename = _dest_filename(src_path, options, dest_dir)
-
       dest_path = File.join(dest_dir, dest_filename)
       dest_path_rel = File.join(CACHE_DIR, dest_filename)
 
       return src_path, dest_path, dest_dir, dest_filename, dest_path_rel
     end
 
-    # Determine whether the image needs to be written.
     def _must_create?(src_path, dest_path)
       !File.exist?(dest_path) || File.mtime(dest_path) <= File.mtime(src_path)
     end
 
-    # Read, process, and write out as new image.
     def _process_img(src_path, options, dest_path)
       image = MiniMagick::Image.open(src_path)
 
       image.auto_orient
       image.resize options
       image.strip
+      image.format "webp"
 
       image.write dest_path
     end
@@ -66,7 +59,7 @@ module Jekyll
       FileUtils.mkdir_p(dest_dir)
 
       if _must_create?(src_path, dest_path)
-        puts "Resizing '#{source}' to '#{dest_path_rel}' - using options: '#{options}'"
+        puts "Resizing '#{source}' to WebP '#{dest_path_rel}' - using options: '#{options}'"
 
         _process_img(src_path, options, dest_path)
 
